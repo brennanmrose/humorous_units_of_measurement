9af0c133845265a56e4cf46ca20963ebac7fa5af
github username <github email address>
2018-12-12 20:23:21 -0500

    update cli

--- lib/humorous_units_of_measurement/scraper.rb
+++ lib/humorous_units_of_measurement/scraper.rb
@@ -7,16 +7,15 @@ class HumorousUnitsOfMeasurement::Scraper
 		get_page = Nokogiri::HTML(open('https://en.wikipedia.org/wiki/List_of_humorous_units_of_measurement')) 
 		attributes = Hash.new 
 		measurements = get_page.css("div.mw-parser-output h2").css("span.mw-headline").children
-		measurements.each do |a|
+		measurements[0..7].each do |a|
 			measurement = a.text 
 			attributes[:measurement] = measurement
 		end
 		terms = get_page.css("div.mw-parser-output h3").css("span.mw-headline").children 
 		terms.each do |b|
 			term = b.text 
-			attributes[:term] = term			
-		binding.pry
-		end 
+			attributes[:term] = term
+		end
 		# array_of_measurements.each do |attr|
 		# 	attributes = {
 				# measurement: attr.name,
