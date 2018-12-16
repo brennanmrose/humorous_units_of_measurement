f9b762771bf4babdfd49674591fe8f5d5009410d
github username <github email address>
2018-12-15 21:52:46 -0500

    add array of nested hashes to scraper

--- lib/humorous_units_of_measurement/cli.rb
+++ lib/humorous_units_of_measurement/cli.rb
@@ -4,36 +4,50 @@ class HumorousUnitsOfMeasurement::CLI
 		puts "Welcome to the Humorous Units of Measurment CLI"
 		puts "Here are the types of measurments you can select from:"
 		HumorousUnitsOfMeasurement::Scraper.scrape_units_of_measurement
+		sort_measurements
 		list_measurements
 		select_measurement
 	end 
 
+	def sort_measurements
+		@sorted_measurements = HumorousUnitsOfMeasurement::Measurements.all.sort_by{|measurement| measurement.category}
+	end
 
 	def list_measurements
-		HumorousUnitsOfMeasurement::Measurements.all.each.with_index(1) do |measurement, index|
-			puts "#{index}. #{measurement.name}" 
+		@sorted_measurements.each.with_index(1) do |measurement, index|
+			puts "#{index}. #{measurement.category}" 
 		end
 	end
 
 	def select_measurement 
-		puts "Please enter the number (1 - 8) of the measurement you would like to learn more about"
+		puts "Please enter the number (1 - 8) of the measurement you would like to learn more about."
 		input = gets.strip
 		index = input.to_i-1 
 		if index.between?(0,8)
-			puts "You have selected #{measurement.name}"
-			puts "Here is a list of humorous terms found under #{measurement.name}:"
-			list_terms
+			measurement = @sorted_measurements[index]
+			puts "You have selected #{measurement.category}"
+			puts "Here is a list of humorous terms found under #{measurement.category}:"
+			binding.pry
+			# terms = @sorted_measurements[index].term.each.with_index(1) do |term, index|
+			# 	terms.delete_if{|key, value| key == "definition" }
+			# 		puts "#{index}. #{term}"
+			# 	end
+			# end
+			# list_terms
 		elsif input == "exit"
 		else 
 			puts "Sorry, that input cannot be found"
-			select_measurement
+			# select_measurement
 		end
 	end 
 
-	def list_terms
-		# HumorousUnitsOfMeasurement::Terms pull terms for each unit
-		select_term
-	end
+	# def list_terms
+	# 	binding.pry
+	# 	@sorted_measurements[index]each.with_index(1) do |term, index|
+	# 		puts "#{index}. #{term}"
+	# 	end
+	# 	# select_term
+	# end
 
 	def select_term
 		puts "If you would like to see the definition of this term, please enter the term number now"
--- lib/humorous_units_of_measurement/scraper.rb
+++ lib/humorous_units_of_measurement/scraper.rb
@@ -17,29 +17,71 @@ class HumorousUnitsOfMeasurement::Scraper
 			terms_array << terms_hash
 		end
 		definitions_array = Array.new
-		definitions = get_page.css("div.mw-parser-output h3 + p").children
-		# binding.pry
+		definitions = get_page.css("div.mw-parser-output h3 + p")
+		definitions_hash ={:definition => []}
 		definitions.each do |c|
 			definition = c.text
-			definitions_hash ={:definition => definition}
-			definitions_array << definitions_hash
+			# definitions_hash ={:definition => definition}
+			definitions_array << definition
 		end
-		binding.pry
 		measurement_one = HumorousUnitsOfMeasurement::Measurements.new(categories_array[0])
 			measurement_one.term = terms_array[0], terms_array[1], terms_array[2]
+				measurement_one.term[0][:definition]  
+				measurement_one.term[1][:definition] = definitions_array[0]
+				measurement_one.term[2][:definition] = definitions_array[1]
 		measurement_two =  HumorousUnitsOfMeasurement::Measurements.new(categories_array[1])
 			measurement_two.term = terms_array[3], terms_array[4]
+				measurement_two.term[0][:definition] = definitions_array[2]
+				measurement_two.term[1][:definition] 
 		measurement_three =  HumorousUnitsOfMeasurement::Measurements.new(categories_array[2])
 			measurement_three.term = terms_array[5], terms_array[6], terms_array[7], terms_array[8], terms_array[9], terms_array[10], terms_array[11], terms_array[12]
+				measurement_three.term[0][:definition] = definitions_array[3]
+				measurement_three.term[1][:definition] = definitions_array[4]
+				measurement_three.term[2][:definition] = definitions_array[5]
+				measurement_three.term[3][:definition] = definitions_array[6]
+				measurement_three.term[4][:definition] = definitions_array[7]
+				measurement_three.term[5][:definition] 
+				measurement_three.term[6][:definition] = definitions_array[8]
+				measurement_three.term[7][:definition] = definitions_array[9]
 		measurement_four =  HumorousUnitsOfMeasurement::Measurements.new(categories_array[3])
 			measurement_four.term = terms_array[13], terms_array[14]
+				measurement_four.term[0][:definition] = definitions_array[10]
+				measurement_four.term[1][:definition] = definitions_array[11]
 		measurement_five =  HumorousUnitsOfMeasurement::Measurements.new(categories_array[4])
 			measurement_five.term = terms_array[15], terms_array[16]
+				measurement_five.term[0][:definition] = definitions_array[12]
+				measurement_five.term[1][:definition] = definitions_array[13]
 		measurement_six =  HumorousUnitsOfMeasurement::Measurements.new(categories_array[5])
 			measurement_six.term = terms_array[17], terms_array[18]
+				measurement_six.term[0][:definition] = definitions_array[14]
+				measurement_six.term[1][:definition] = definitions_array[15]
 		measurement_seven =  HumorousUnitsOfMeasurement::Measurements.new(categories_array[6])
 			measurement_seven.term = terms_array[19], terms_array[20], terms_array[21], terms_array[22], terms_array[23], terms_array[24], terms_array[25], terms_array[26]
+				measurement_seven.term[0][:definition] = definitions_array[16]
+				measurement_seven.term[1][:definition] 
+				measurement_seven.term[2][:definition] = definitions_array[17]
+				measurement_seven.term[3][:definition] = definitions_array[18]
+				measurement_seven.term[4][:definition] = definitions_array[19]
+				measurement_seven.term[5][:definition] = definitions_array[20]
+				measurement_seven.term[6][:definition] = definitions_array[21]
+				measurement_seven.term[7][:definition] = definitions_array[22]
 		measurement_eight =  HumorousUnitsOfMeasurement::Measurements.new(categories_array[7])
-			measurement_eight.term = terms_array[26], terms_array[28], terms_array[29], terms_array[30], terms_array[31], terms_array[32], terms_array[33], terms_array[34], terms_array[35], terms_array[36], terms_array[37], terms_array[38]
+			measurement_eight.term = terms_array[27], terms_array[28], terms_array[29], terms_array[30], terms_array[31], terms_array[32], terms_array[33], terms_array[34], terms_array[35], terms_array[36], terms_array[37], terms_array[38]
+				measurement_eight.term[0][:definition] = definitions_array[23]
+				measurement_eight.term[1][:definition] = definitions_array[24]
+				measurement_eight.term[2][:definition] 
+				measurement_eight.term[3][:definition] = definitions_array[25]
+				measurement_eight.term[4][:definition] = definitions_array[26]
+				measurement_eight.term[5][:definition] = definitions_array[27]
+				measurement_eight.term[6][:definition] = definitions_array[28]
+				measurement_eight.term[7][:definition] = definitions_array[29]
+				measurement_eight.term[8][:definition] = definitions_array[30]
+				measurement_eight.term[9][:definition] = definitions_array[31]
+				measurement_eight.term[10][:definition] = definitions_array[32]
+				measurement_eight.term[11][:definition] = definitions_array[33]
 	end
 end
+
+			# definition = c.text
+			# definitions_hash[:definition] << definition
+			# definitions_array << definitions_hash
